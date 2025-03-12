f707e7315f3b86e640cfdcc7eb10ae0547e68fb6
Saurabh <saurabhkg36@gmail.com>
2025-03-12 15:24:05 +0530

    week 1 and 2

---
+++ Week2/Code/wikitableextract.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+chrome_driver_path = r"C:\Users\HP\Desktop\ApexaiQ\chromedriver.exe"
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://en.wikipedia.org/wiki/Java_version_history")
+
+time.sleep(3)
+table = driver.find_element(By.XPATH, "//table[contains(@class,'wikitable')]")
+rows = table.find_elements(By.TAG_NAME, "tr")
+
+table_data = []
+
+for row in rows:
+    
+    headers = row.find_elements(By.TAG_NAME, "th")
+    if headers:  
+        row_data = [col.text.strip() for col in headers]
+        table_data.append(row_data)
+        continue  
+    
+    cols = row.find_elements(By.TAG_NAME, "td")
+    row_data = [col.text.strip() for col in cols]
+    
+    if row_data:
+        table_data.append(row_data)
+        
+df = pd.DataFrame(table_data)
+
+df.to_csv("./extracted/Java_versions.csv",index=False)
+
+
+print('Tables extracted successfully')
+driver.quit()
